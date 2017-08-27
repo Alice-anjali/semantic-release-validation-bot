5db37de6e6dd74253db60bf1d66d18434a3e57c7
Alice-anjali <alice.anjali1995@gmail.com>
2017-08-27 19:00:19 +0530

    edit pr

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 module.exports = probotPlugin
 
 const handlePullRequestChange = require('./lib/handle-pull-request')
+const handlePullRequestEdit = require('./lib/handle-pull-request-edit.js')
 
 function probotPlugin (robot) {
   robot.on('pull_request.opened', handlePullRequestChange.bind(null, robot))
+  robot.on('pull_request.edited', handlePullRequestEdit.bind(null, robot))
 }
