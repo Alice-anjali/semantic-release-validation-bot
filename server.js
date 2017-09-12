b3ef4a768c572ffbf27d46f3bd04df978d56113d
Alice-anjali <alice.anjali1995@gmail.com>
2017-09-12 17:20:13 +0530

    updates file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 module.exports = probotPlugin
 
 const handlePullRequestChange = require('./lib/handle-pull-request')
-const handlePullRequestEdit = require('./lib/handle-pull-request-edit.js')
+const handlePullRequestEdit = require('./lib/handle-pull-request-edit')
 
 function probotPlugin (robot) {
   robot.on('pull_request.opened', handlePullRequestChange.bind(null, robot))
